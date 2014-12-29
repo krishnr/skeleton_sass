6cbc571da27fd83fc35c3d39060ba6f4df3045e9
Krishn Ramesh <krishnr@gmail.com>
2014-12-28 23:42:09 -0500

    Added skeleton sass file and created Rails engine

--- lib/skeleton_sass.rb
+++ lib/skeleton_sass.rb
@@ -1,5 +1,9 @@
 require "skeleton_sass/version"
 
 module SkeletonSass
-  # Your code goes here...
+  module Rails
+    class Engine < ::Rails::Engine
+      # The engine gets rails to add app, lib, vendor folders to load path of the larger application
+    end
+  end
 end
